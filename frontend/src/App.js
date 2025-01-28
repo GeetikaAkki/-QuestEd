7ded53a686c6b4e85643694308dd8f00eefde148
bunnysunny24 <bhavashesh@gmail.com>
2025-01-29 01:13:08 +0530

    Working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes,Route } from 'react-router-dom'
-import AuthForm from './components/StudSign'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthForm from './components/StudSign';
 import LogIn from './components/Login';
 
 function App() {
   return (
-   <div>
-    <BrowserRouter>
-    <Routes>
-      <Route path ='/sign-in' element={<AuthForm/>}></Route>
-      <Route path ='/log-in' element={<LogIn/>}></Route>
-    </Routes>
-    </BrowserRouter>
-   </div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthForm />} />
+          <Route path="/sign-in" element={<AuthForm />} />
+          <Route path="/log-in" element={<LogIn />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
