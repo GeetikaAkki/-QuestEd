e54e8b290d6f19ce9b99d38706ecd6a835d20a0d
Geetika Akki <geetika.akki@gmail.com>
2025-01-29 00:32:08 +0530

    Sign In and Log In UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import AuthForm from './components/StudSign'
+import LogIn from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path ='/sign-in' element={<AuthForm/>}></Route>
+      <Route path ='/log-in' element={<LogIn/>}></Route>
+    </Routes>
+    </BrowserRouter>
+   </div>
   );
 }
 
